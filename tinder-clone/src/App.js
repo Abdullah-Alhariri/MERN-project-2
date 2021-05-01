c64fb9dd54a83593b00d9229a5ec9115069b314e
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2021-05-01 02:30:50 +0200

    header

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h1>I'm the content of App.js</h1>
+      {/* 2) TinderCards */}
+      {/* 3) SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
